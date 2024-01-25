89fede31a1f6d462f6e428463de23f92319cd403
UK <jsw4795@naver.com>
2024-01-25 14:37:24 +0900

    문제 설명 추가

--- src/programmers/lv0/문자열_반복해서_출력하기/Solution.java
+++ src/programmers/lv0/문자열_반복해서_출력하기/Solution.java
@@ -1,3 +1,16 @@
+/* Scanner를 통해 문자열과 숫자가 입력된다
+ * 
+ * 입력된 숫자만큼 문자열을 반복하여 출력하라
+ * 
+ * 예)
+ * 문자열 : abc
+ * 숫자 : 3
+ * 
+ * 출력 : abcabcabc
+ * */
+
+
+
 package programmers.lv0.문자열_반복해서_출력하기;
 
 import java.util.Scanner;
